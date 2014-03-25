7b7b53684bd67207389a7a6369e7ec950240d554
Riobard <me@riobard.com>
2014-03-24 22:54:41 -0230

    Revert "Revert "Merge pull request #70 from riobard/refactor/vbx""
    This reverts commit 0c4effd44d8dd15b0c9a30d47cbd6d68b7183cbf.

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
-// This is the boot2docker management utilty.
 package main
 
-import (
-	"os"
-
-	// keep 3rd-party imports separate from stdlib with an empty line
-	flag "github.com/ogier/pflag"
-)
+import "os"
 
 // The following vars will be injected during the build process.
 var (
@@ -19,20 +13,19 @@ func main() {
 	// any deferred cleanup statements. It might cause unintended effects. To
 	// be safe, we wrap the program in run() and only os.Exit() outside the
 	// wrapper. Be careful not to indirectly trigger os.Exit() in the program,
-	// notably via log.Fatal().
+	// notably via log.Fatal() and on flag.Parse() where the default behavior
+	// is ExitOnError.
 	os.Exit(run())
 }
 
 // Run the program and return exit code.
 func run() int {
-	flag.Usage = usageLong // make "-h" work similarly to "help"
-
-	if err := config(); err != nil {
-		errf("%s\n", err)
+	flags, err := config()
+	if err != nil {
 		return 1
 	}
 
-	switch cmd := flag.Arg(0); cmd {
+	switch cmd := flags.Arg(0); cmd {
 	case "download":
 		return cmdDownload()
 	case "init":
@@ -61,7 +54,7 @@ func run() int {
 		outf("Client version: %s\nGit commit: %s\n", Version, GitSHA)
 		return 0
 	case "help":
-		flag.Usage()
+		flags.Usage()
 		return 0
 	case "":
 		usageShort()
@@ -71,35 +64,4 @@ func run() int {
 		usageShort()
 		return 1
 	}
-}
-
-func usageShort() {
-	errf("Usage: %s [<options>] {help|init|up|ssh|save|down|poweroff|reset|restart|status|info|delete|download|version} [<args>]\n", os.Args[0])
-
-}
-
-func usageLong() {
-	// NOTE: the help message uses spaces, not tabs for indentation!
-	errf(`boot2docker management utility.
-
-Usage: %s [<options>] <command> [<args>]
-
-Commands:
-    init [<vm>]             Create a new boot2docker VM.
-    up|start|boot [<vm>]    Start VM from any states.
-    ssh                     Login to VM via SSH.
-    save|suspend [<vm>]     Suspend VM and save state to disk.
-    down|stop|halt [<vm>]   Gracefully shutdown the VM.
-    restart [<vm>]          Gracefully reboot the VM.
-    poweroff [<vm>]         Forcefully power off the VM (might corrupt disk image).
-    reset [<vm>]            Forcefully power cycle the VM (might corrupt disk image).
-    delete [<vm>]           Delete boot2docker VM and its disk image.
-    info [<vm>]             Display detailed information of VM.
-    status [<vm>]           Display current state of VM.
-    download                Download boot2docker ISO image.
-    version                 Display version information.
-
-Options:
-`, os.Args[0])
-	flag.PrintDefaults()
 }
---
+++ virtualbox/storage.go
@@ -0,0 +1,51 @@
+package virtualbox
+
+// StorageController represents a virtualized storage controller.
+type StorageController struct {
+	SysBus      SystemBus
+	Ports       uint // SATA port count 1--30
+	Chipset     StorageControllerChipset
+	HostIOCache bool
+	Bootable    bool
+}
+
+// SystemBus represents the system bus of a storage controller.
+type SystemBus string
+
+const (
+	SysBusIDE    = SystemBus("ide")
+	SysBusSATA   = SystemBus("sata")
+	SysBusSCSI   = SystemBus("scsi")
+	SysBusFloppy = SystemBus("floppy")
+)
+
+// StorageControllerChipset represents the hardware of a storage controller.
+type StorageControllerChipset string
+
+const (
+	CtrlLSILogic    = StorageControllerChipset("LSILogic")
+	CtrlLSILogicSAS = StorageControllerChipset("LSILogicSAS")
+	CtrlBusLogic    = StorageControllerChipset("BusLogic")
+	CtrlIntelAHCI   = StorageControllerChipset("IntelAHCI")
+	CtrlPIIX3       = StorageControllerChipset("PIIX3")
+	CtrlPIIX4       = StorageControllerChipset("PIIX4")
+	CtrlICH6        = StorageControllerChipset("ICH6")
+	CtrlI82078      = StorageControllerChipset("I82078")
+)
+
+// StorageMedium represents the storage medium attached to a storage controller.
+type StorageMedium struct {
+	Port      uint
+	Device    uint
+	DriveType DriveType
+	Medium    string // none|emptydrive|<uuid>|<filename|host:<drive>|iscsi
+}
+
+// DriveType represents the hardware type of a drive.
+type DriveType string
+
+const (
+	DriveDVD = DriveType("dvddrive")
+	DriveHDD = DriveType("hdd")
+	DriveFDD = DriveType("fdd")
+)
