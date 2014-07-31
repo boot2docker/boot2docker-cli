ba76cc87356278c78096a0b4831106fc3606f90b
Sven Dowideit <SvenDowideit@docker.com>
2014-07-31 12:57:40 +1000

    Revert "Revert "Create driver model and refactor virtualbox dependencies into dr..."

--- virtualbox/vbm.go
+++ virtualbox/vbm.go
@@ -3,6 +3,7 @@ package virtualbox
 import (
 	"bytes"
 	"errors"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -10,6 +11,8 @@ import (
 	"regexp"
 	"runtime"
 	"strings"
+
+	"github.com/boot2docker/boot2docker-cli/driver"
 )
 
 var (
@@ -85,4 +88,90 @@ func vbmOutErr(args ...string) (string, string, error) {
 		}
 	}
 	return stdout.String(), stderr.String(), err
+}
+
+// Get or create the hostonly network interface
+func getHostOnlyNetworkInterface(mc *driver.MachineConfig) (string, error) {
+	// Check if the interface/dhcp exists.
+	nets, err := HostonlyNets()
+	if err != nil {
+		return "", err
+	}
+
+	dhcps, err := DHCPs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, n := range nets {
+		if dhcp, ok := dhcps[n.NetworkName]; ok {
+			if dhcp.IPv4.IP.Equal(mc.DHCPIP) &&
+				dhcp.IPv4.Mask.String() == mc.NetMask.String() &&
+				dhcp.LowerIP.Equal(mc.LowerIP) &&
+				dhcp.UpperIP.Equal(mc.UpperIP) &&
+				dhcp.Enabled == mc.DHCPEnabled {
+				return n.Name, nil
+			}
+		}
+	}
+
+	// No existing host-only interface found. Create a new one.
+	hostonlyNet, err := CreateHostonlyNet()
+	if err != nil {
+		return "", err
+	}
+	hostonlyNet.IPv4.IP = mc.HostIP
+	hostonlyNet.IPv4.Mask = mc.NetMask
+	if err := hostonlyNet.Config(); err != nil {
+		return "", err
+	}
+
+	// Create and add a DHCP server to the host-only network
+	dhcp := driver.DHCP{}
+	dhcp.IPv4.IP = mc.DHCPIP
+	dhcp.IPv4.Mask = mc.NetMask
+	dhcp.LowerIP = mc.LowerIP
+	dhcp.UpperIP = mc.UpperIP
+	dhcp.Enabled = true
+	if err := AddHostonlyDHCP(hostonlyNet.Name, dhcp); err != nil {
+		return "", err
+	}
+	return hostonlyNet.Name, nil
+}
+
+// Copy disk image from given source path to destination
+func copyDiskImage(dst, src string) (err error) {
+	// Open source disk image
+	srcImg, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if ee := srcImg.Close(); ee != nil {
+			err = ee
+		}
+	}()
+	dstImg, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if ee := dstImg.Close(); ee != nil {
+			err = ee
+		}
+	}()
+	_, err = io.Copy(dstImg, srcImg)
+	return err
+}
+
+// Make a boot2docker VM disk image with the given size (in MB).
+func makeDiskImage(dest string, size uint, initialBytes []byte) error {
+	// Create the dest dir.
+	if err := os.MkdirAll(filepath.Dir(dest), 0755); err != nil {
+		return err
+	}
+	// Fill in the magic string so boot2docker VM will detect this and format
+	// the disk upon first boot.
+	raw := bytes.NewReader(initialBytes)
+	return MakeDiskImage(dest, size, raw)
 }
