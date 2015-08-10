35e2fc49f5212fdb4f49620d7c4847cfa3b40d68
Nathan LeClaire <nathan.leclaire@gmail.com>
2015-08-10 14:50:14 -0700

    Make error message configurable by HTTP call
    Signed-off-by: Nathan LeClaire <nathan.leclaire@gmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Deprecated
 
-This project is officially deprecated in favor of [Docker Machine](https://docs.docker.com/machine/).  The code and documentation here only exist as a reference for users who have not yet switched over (but please do soon).
+This project is officially deprecated in favor of [Docker
+Machine](https://docs.docker.com/machine/).  The code and documentation here
+only exist as a reference for users who have not yet switched over (but please
+do soon).  The recommended way to install Machine is with the [Docker
+Toolbox](https://docker.com/toolbox).
 
 # boot2docker command line management tool
 
--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
-	"path/filepath"
 )
 
 // The following vars will be injected during the build process.
 var (
 	Version string
 	GitSHA  string
+)
+
+const (
+	hardcodedWarning = `
+        WARNING: The 'boot2docker' command line interface is being officially deprecated.
+	Users are expected to switch to Docker Machine (https://docs.docker.com/machine/) instead ASAP.
+	The Docker Toolbox is the recommended way to install it: https://docker.com/toolbox/
+
+`
+	warningURL = "https://raw.githubusercontent.com/boot2docker/boot2docker-cli/master/DEPRECATION_WARNING"
 )
 
 type unknownCommandError struct {
@@ -21,8 +32,6 @@ func (e unknownCommandError) Error() string {
 }
 
 func main() {
-	fmt.Fprintf(os.Stderr, "\n  WARNING: the %q command is officially deprecated.\n    Please switch to Docker Machine (https://docs.docker.com/machine/)\n    as soon as possible.\n\n", filepath.Base(os.Args[0]))
-
 	// os.Exit will terminate the program at the place of call without running
 	// any deferred cleanup statements. It might cause unintended effects. To
 	// be safe, we wrap the program in run() and only os.Exit() outside the
@@ -51,8 +60,10 @@ func run() error {
 	case "config", "cfg":
 		return cmdConfig()
 	case "init":
+		printDeprecationWarning()
 		return cmdInit()
 	case "up", "start", "boot", "resume":
+		printDeprecationWarning()
 		return cmdUp()
 	case "save", "suspend":
 		return cmdSave()
@@ -90,4 +101,28 @@ func run() error {
 	default:
 		return unknownCommandError{cmd: cmd}
 	}
+}
+
+func printDeprecationWarning() {
+	var (
+		warning string
+	)
+
+	// Try to get the warning from the Github raw URL.  If there's any
+	// failure along the way, e.g. network, just fall back to the default
+	// warning hardcoded in the source.
+	resp, err := http.Get(warningURL)
+	if err != nil || resp.StatusCode != http.StatusOK {
+		warning = hardcodedWarning
+	} else {
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			warning = hardcodedWarning
+		} else {
+			warning = string(body)
+		}
+	}
+
+	fmt.Fprintln(os.Stderr, warning)
 }
